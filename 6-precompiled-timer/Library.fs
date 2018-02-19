21b928491fb2049fa680870db2cfcce9f8d22a4c
Mikhail Shilkov <mshilkov@astrata.eu>
2018-02-19 11:25:31 +0100

    Cleanup

--- 6-precompiled-timer/Library.fs
+++ 6-precompiled-timer/Library.fs
@@ -1,9 +1,16 @@
 namespace HelloFunctions
 
+open System
 open Microsoft.Azure.WebJobs
 open Microsoft.Azure.WebJobs.Host
 
 module Say =
+  let private daysUntil (d: DateTime) =
+    (d - DateTime.Now).TotalDays |> int
+
   let hello (timer: TimerInfo, log: TraceWriter) =
-    let now = System.DateTime.Now.ToString("HH:mm:ss dd/MM/yyyy")
-    log.Info (sprintf "Function ran at %s" now)
+    let christmas = new DateTime(2017, 12, 25)
+
+    daysUntil christmas
+    |> sprintf "%d days until Christmas"
+    |> log.Info
--- 9-attributes/PrecompiledHttp.fs
+++ 9-attributes/PrecompiledHttp.fs
@@ -4,11 +4,13 @@ open Microsoft.AspNetCore.Mvc
 open Microsoft.AspNetCore.Http
 open Microsoft.Azure.WebJobs
 open Microsoft.Azure.WebJobs.Host
+open Microsoft.Azure.WebJobs.Extensions.Http
 
 module AttributeBased =
 
   [<FunctionName("AttributeBased")>]
-  let run([<HttpTrigger>] req: HttpRequest, log: TraceWriter) =
+  let run([<HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "hellosanta")>] req: HttpRequest, log: TraceWriter, context: ExecutionContext) =
     log.Info("F# HTTP trigger function processed a request.")
+    log.Info context.FunctionDirectory
 
     ContentResult(Content = "function.json was successfully auto-generated.", ContentType = "text/html")
